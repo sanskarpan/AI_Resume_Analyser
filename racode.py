78108fc904a24e11c0ed67d7fce0e19f2710afc4
Sanskar Pandey <142474055+sanskarpan@users.noreply.github.com>
2024-03-29 19:49:52 +0530

    Update racode.py

--- racode.py
+++ racode.py
@@ -1,15 +1,10 @@
 import streamlit as st
 from dotenv import load_dotenv
-from openai import OpenAI 
+import openai
 import os
+
 load_dotenv()
-
-
-client = OpenAI(
-  api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
-)
-
-
+openai.api_key = os.getenv('api_key')
 st.title("Resume Feedback Application")
 
 uploaded_file = st.file_uploader("Upload your resume", type=["pdf", "docx"])
@@ -21,13 +16,15 @@ if uploaded_file is not None:
         resume_text = uploaded_file.read().decode('latin-1')
 
     try:
-        feedback = client.chat.completions.create(
-            model="gpt-3.5-turbo-instruct",
-            messages=[{"role": "user", "content":"Provide feedback on this resume:\n{resume_text}"}],
-            temperature =0,          
+        feedback = openai.Completion.create(
+            engine="gpt-3.5-turbo-instruct",  
+            prompt=f"Provide feedback on this resume:\n{resume_text}\n---\n",
+            temperature=0.5,
+            max_tokens=400
         )
-    except ZeroDivisionError:
-        st.error(f"OpenAI API error")  
+    except openai.error.InvalidRequestError as e:
+        st.error(f"OpenAI API error: {str(e)}")  
     else:
         st.subheader("Feedback on your resume:")
-        st.write(feedback.choices[0].message.content)
+        st.write(feedback['choices'][0]['text'].strip())
+
